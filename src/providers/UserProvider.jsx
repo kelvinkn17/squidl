14f43eeebfa078949c355a704ee79a7d72a5de1c
tfarhan00 <farhanalryan2@gmail.com>
2024-10-30 20:00:24 +0700

    fix: add total balance in dashboard

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -6,64 +6,65 @@ import { useAuth } from "./AuthProvider";
 import toast from "react-hot-toast";
 import { useLocalStorage } from "@uidotdev/usehooks";
 
-
 const UserContext = createContext({
-  assets: {}
-})
+  assets: {},
+  userData: {},
+});
 
 export default function UserProvider({ children }) {
-  const { isSignedIn } = useSession()
-  const { userData } = useAuth()
-  const [assets, setAssets] = useLocalStorage('user-assets', null)
+  const { isSignedIn } = useSession();
+  const { userData } = useAuth();
+  const [assets, setAssets] = useLocalStorage("user-assets", null);
 
   const handleFetchAssets = async () => {
     try {
       console.log(`Fetching assets for ${userData.username}.squidl.eth`, {
-        userData
-      })
+        userData,
+      });
 
       const res = await squidlAPI.get(
         `/user/wallet-assets/${userData.username}.squidl.eth/assets`
-      )
+      );
 
-      console.log('User assets', res.data)
-      setAssets(res.data)
+      console.log("User assets", res.data);
+      setAssets(res.data);
     } catch (error) {
-      console.error('Error fetching user assets', error)
-      toast.error('Error fetching user assets')
+      console.error("Error fetching user assets", error);
+      toast.error("Error fetching user assets");
     }
-  }
+  };
 
   useEffect(() => {
-    console.log('assets', assets)
-  }, [assets])
+    console.log("assets", assets);
+  }, [assets]);
 
   useEffect(() => {
     if (isSignedIn && userData && userData?.username) {
       // Fetch assets every 10 seconds
-      handleFetchAssets()
+      handleFetchAssets();
 
       const interval = setInterval(() => {
-        handleFetchAssets()
-      }, 20000)
+        handleFetchAssets();
+      }, 20000);
 
       return () => {
-        clearInterval(interval)
-      }
+        clearInterval(interval);
+      };
     }
-  }, [isSignedIn, userData])
+  }, [isSignedIn, userData]);
 
   return (
     <UserContext.Provider
       value={{
-        assets: assets
+        assets: assets,
+        userData: userData,
       }}
     >
       {children}
     </UserContext.Provider>
-  )
+  );
 }
 
 export const useUser = () => {
-  return useContext(UserContext)
-}
+  return useContext(UserContext);
+};
