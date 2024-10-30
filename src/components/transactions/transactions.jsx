3400d47a3237ceb0768c32b7e85e99b82075a74a
buwono <buwonodirga@gmail.com>
2024-10-30 17:49:50 +0700

    feat(transactions): implement transactions api

--- src/components/transactions/transactions.jsx
+++ src/components/transactions/transactions.jsx
@@ -1,8 +1,55 @@
-import { shortenAddress } from "../../utils/string.js";
+import useSWR from "swr";
 import TxItem from "../alias/TxItem.jsx";
 import { Icons } from "../shared/Icons.jsx";
+import { squidlAPI } from "../../api/squidl.js";
+import { useEffect, useMemo } from "react";
+import { format } from "date-fns";
+import { formatCurrency, shortenId } from "../../utils/formatting-utils.js";
+import { Spinner } from "@nextui-org/react";
 
 export default function Transactions() {
+  const { data: user, isLoading } = useSWR("/auth/me", async (url) => {
+    const { data } = await squidlAPI.get(url);
+    return data;
+  });
+
+  const shouldFetchTransactions = user?.username ? true : false;
+
+  const {
+    data: transactionsData,
+    isLoading: isLoadingTransactionsData,
+    mutate: mutateTransactionsData,
+  } = useSWR(
+    shouldFetchTransactions
+      ? `/user/wallet-assets/${user.username}.squidl.me/transactions`
+      : null,
+    async (url) => {
+      const { data } = await squidlAPI.get(url);
+      return data;
+    }
+  );
+
+  const groupedTransactions = useMemo(() => {
+    return transactionsData?.reduce((acc, tx) => {
+      const dateKey = format(new Date(tx.createdAt), "MM/dd/yyyy");
+      if (!acc[dateKey]) {
+        acc[dateKey] = [];
+      }
+      acc[dateKey].push(tx);
+      return acc;
+    }, {});
+  }, [transactionsData]);
+
+  useEffect(() => {
+    if (!mutateTransactionsData) return;
+
+    const interval = setInterval(() => {
+      mutateTransactionsData();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [mutateTransactionsData]);
+
   return (
     <div
       className={
@@ -16,48 +63,63 @@ export default function Transactions() {
         </div>
       </div>
 
-      <p className="text-[#A1A1A3] font-medium text-sm mt-1">09/20/2024</p>
-
-      <div className="flex flex-col w-full">
-        <TxItem
-          tokenImg={"/assets/eth-logo.png"}
-          chainImg={"/assets/eth-logo.png"}
-          title={"trivial.jane.squidl.me - Deposit"}
-          isNounsies
-          addressNounsies={"0x02919065a8Ef7A782Bb3D9f3DEFef2FA0a4d1f34"}
-          subtitle={`from ${shortenAddress(
-            "0x02919065a8Ef7A782Bb3D9f3DEFef2FA0a4d1f34"
-          )}`}
-          value={"0.512 USDC"}
-          subValue={"$33912"}
+      {isLoadingTransactionsData ? (
+        <Spinner
+          size="md"
+          color="primary"
+          className="flex items-center justify-center w-full h-40"
         />
-
-        <TxItem
-          tokenImg={"/assets/eth-logo.png"}
-          chainImg={"/assets/eth-logo.png"}
-          title={"xx.jane.squidl.me - Deposit"}
-          isNounsies
-          addressNounsies={"0x02919065a8Ef7A782Bb3D9f3DEFef2F230a4d1f34"}
-          subtitle={`from ${shortenAddress(
-            "0x02919065a8Ef7A782Bb3D9f3DEFef2FA0a4d1f32"
-          )}`}
-          value={"0.512 USDC"}
-          subValue={"$33912"}
-        />
-
-        <TxItem
-          tokenImg={"/assets/eth-logo.png"}
-          chainImg={"/assets/eth-logo.png"}
-          title={"yy.jane.squidl.me - Deposit"}
-          isNounsies
-          addressNounsies={"0x02919065a8Ef7A782Bb3D9f3134Fef2FA0a4d1f34"}
-          subtitle={`from ${shortenAddress(
-            "0x02919065a8Ef7A782Bb3D9f3DEFef2FA0a4d1f35"
-          )}`}
-          value={"0.512 USDC"}
-          subValue={"$33912"}
-        />
-      </div>
+      ) : transactionsData && transactionsData.length > 0 ? (
+        <div className="flex flex-col w-full">
+          {Object.keys(groupedTransactions).map((date) => (
+            <div key={date} className="mb-4">
+              <p className="text-[#A1A1A3] font-medium text-sm mt-1">{date}</p>
+              {groupedTransactions[date].map((tx, idx) => {
+                return (
+                  <TxItem
+                    key={idx}
+                    isNounsies
+                    addressNounsies={
+                      tx.stealthAddress.alias.alias === "" ||
+                      tx.stealthAddress.alias.alias === null
+                        ? "1"
+                        : tx.stealthAddress.alias.alias
+                    }
+                    chainImg={
+                      tx.chain.imageUrl
+                        ? tx.chain.imageUrl
+                        : "/assets/line-logo.png"
+                    }
+                    title={
+                      tx.stealthAddress.alias.alias === "" ||
+                      tx.stealthAddress.alias.alias === null
+                        ? "Unknown"
+                        : tx.stealthAddress.alias.alias
+                    }
+                    subtitle={`from ${shortenId(tx.fromAddress)}`}
+                    value={formatCurrency(
+                      tx.amount,
+                      tx.isNative
+                        ? tx.chain.nativeToken.symbol
+                        : tx.token.symbol
+                    )}
+                    subValue={formatCurrency(
+                      (tx.isNative
+                        ? tx.chain.nativeToken.priceUSD
+                        : tx.token.stats.priceUSD) * tx.amount,
+                      "USD"
+                    )}
+                  />
+                );
+              })}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full flex items-center justify-center min-h-64">
+          No transactions found
+        </div>
+      )}
     </div>
   );
 }
